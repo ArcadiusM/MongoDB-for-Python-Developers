53e74487ecaf81bca1565293d671ea2cda70cc8b
Arkadiusz Modzelewski <arcadius.modzelewski@gmail.com>
2019-12-13 22:11:01 +0100

    Update run.py

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ config.read(os.path.abspath(os.path.join(".ini")))
 if __name__ == "__main__":
     app = create_app()
     app.config['DEBUG'] = True
-    app.config['MFLIX_DB_URI'] = config['PROD']['MFLIX_DB_URI']
-    app.config['MFLIX_NS'] = config['PROD']['MFLIX_NS']
-    app.config['SECRET_KEY'] = config['PROD']['SECRET_KEY']
+    app.config['MFLIX_DB_URI'] = 'mongodb+srv://m220student:m220password@mflix-ogokv.mongodb.net/test?retryWrites=true&w=majority'
+    app.config['MFLIX_NS'] = 'sample_mflix'
+    app.config['SECRET_KEY'] = 'XYZ_super_secret_testing_key_Arcadius_XYZ'
 
     app.run()
